3c86849f8223f86687d3414fc0e46ef4cec2028e
arian <ariannargesi@gmail.com>
2020-08-13 00:25:50 +0430

    game state decleared

---
+++ src/state/index.js
@@ -0,0 +1,10 @@
+// contain the current state of the game 
+const state = {
+    playerScore: 0,
+    gameSpeed: 1,
+    gameInPlay: false,
+    gameOver: false,
+    animationReapet: null
+}
+
+export default state 
