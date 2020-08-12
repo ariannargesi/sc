52004e74d8abf7ff1b0b688214e54d5b173042af
arian <ariannargesi@gmail.com>
2020-08-13 00:25:35 +0430

    start functionality added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 import getMicSoundLevel from './audio'
-
+import state from './state'
+//always contain the latest value of microphone sound level
 let soundLevel = 0
 getMicSoundLevel( x => {
   if(x) 
       soundLevel = x  
 })
+document.addEventListener("keydown", e => {
+  const keyCode = e.keyCode 
+  if(keyCode === 32){
+    startGame()
+  }
+})
+const startGame = () => {
+  state.gameInPlay = true 
+  requestAnimationFrame(update)
+}
+
+const update = () => {
+  console.log("update function")
+  requestAnimationFrame(update)
+}
+
+
+
+
+
 
