49c9fd9d26c437709cd199c51ae458dd7dcdb042
arian <ariannargesi@gmail.com>
2020-08-14 13:14:27 +0430

    collision to edges detected

--- src/app.js
+++ src/app.js
@@ -1,29 +1,37 @@
 import getMicSoundLevel from './audio'
-import state from './state'
+import states from './state'
+import nodes from './domNodes'
+
 //always contain the latest value of microphone sound level
 let soundLevel = 0
 getMicSoundLevel( x => {
   if(x) 
-      soundLevel = x  
+      soundLevel = x * states.sensitivity
 })
-document.addEventListener("keydown", e => {
-  const keyCode = e.keyCode 
-  if(keyCode === 32){
-    startGame()
-  }
+
+document.addEventListener('keydown', e => {
+    const key = e.keyCode 
+    if(key === 32){
+      startGame()
+    }
 })
+
 const startGame = () => {
-  state.gameInPlay = true 
-  requestAnimationFrame(update)
+    states.gameInPlay = true 
+    requestAnimationFrame(updateScreen)
 }
 
-const update = () => {
-  console.log("update function")
-  requestAnimationFrame(update)
+const updateScreen = () => {
+  if(!states.gameOver){    
+    const containerHeight = Math.round(nodes.container.getBoundingClientRect().height)
+    const characterOffsetTop = nodes.character.offsetTop
+    if(soundLevel < 400 && characterOffsetTop < containerHeight - 50 ) {
+        nodes.character.style.top = characterOffsetTop +4 + "px"
+    } 
+    else if(soundLevel > 400 && characterOffsetTop > 0){
+      nodes.character.style.top = characterOffsetTop -4 + "px"
+    }
+    requestAnimationFrame(updateScreen)
+  }
 }
-
-
-
-
-
 
