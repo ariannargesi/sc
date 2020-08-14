a8849ac22c16831f95272ec80d826ea5d75ba3e5
arian <ariannargesi@gmail.com>
2020-08-14 16:49:27 +0430

    add score

--- src/domNodes/index.js
+++ src/domNodes/index.js
@@ -1,6 +1,7 @@
 const nodes = {
     container: document.querySelector('.container'),
-    character: document.querySelector('.character')
+    character: document.querySelector('.character'),
+    score: document.querySelector('.score')
 }
 
 export default nodes 
