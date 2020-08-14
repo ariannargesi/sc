0c369b0e440739db1dbce2b7dde4f73bbbab9e47
arian <ariannargesi@gmail.com>
2020-08-14 19:28:42 +0430

    lock added

--- src/state/index.js
+++ src/state/index.js
@@ -1,10 +1,11 @@
 // contain the current state of the game 
 const states = {
     playerScore: 0,
-    gameSpeed: 1,
+    gameSpeed: 3,
     gameInPlay: false,
     gameOver: false,
-    sensitivity: 0.05
+    sensitivity: 0.04,
+    lock: false 
 }
 
 export default states
