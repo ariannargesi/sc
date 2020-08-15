5adad14ddcb80c9db9d4ba46cffbe1e590fa58ae
arian <ariannargesi@gmail.com>
2020-08-15 18:17:47 +0430

    generate dynamic height for hedges

--- src/hedges/index.js
+++ src/hedges/index.js
@@ -8,6 +8,19 @@ const createHedges = () => {
     hedgeBottom.setAttribute('class', 'hedge')
     
     hedgeBottom.style.bottom = 0 
+    
+    let containerHeight = nodes.container.getBoundingClientRect().height 
+    containerHeight = Math.round(containerHeight)
+    
+    const passageHeight = 200
+
+    let topHedgeheight = Math.random() * (containerHeight * 0.4)  + 70
+    topHedgeheight = Math.round(topHedgeheight)
+    hedgeTop.style.height = topHedgeheight + 'px'
+    hedgeTop.style.transform = 'rotate(180deg)'
+
+    let bottomHedgeHeight = containerHeight - (passageHeight + topHedgeheight)
+    hedgeBottom.style.height = bottomHedgeHeight + 'px'
 
     nodes.container.appendChild(hedgeTop)
     nodes.container.appendChild(hedgeBottom)
