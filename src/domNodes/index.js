b74de9ccfd6dd5296c24a32845ae37fee50ef61c
arian <ariannargesi@gmail.com>
2020-08-18 22:06:32 +0430

    add input slider

--- src/domNodes/index.js
+++ src/domNodes/index.js
@@ -1,10 +1,11 @@
+
 const nodes = {
     container: document.querySelector('.container'),
     character: document.querySelector('.character'),
     score: document.querySelector('.score'),
     startGame: document.querySelector('.start-game'),
-    soundLevelNum: document.querySelector('#sound-level-number'),
-    soundLevelDirection: document.querySelector('#sound-level-direction'),
+    soundLevelNum: document.querySelector('.sound-level-number'),
+    inputSlider: document.querySelector('.slider')
 }
 
 export default nodes
