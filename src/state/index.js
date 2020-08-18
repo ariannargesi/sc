33116ae2675d504e11c7bf962a2f27506db77b75
arian <ariannargesi@gmail.com>
2020-08-18 21:21:47 +0430

    remove gameSpeed interval

--- src/state/index.js
+++ src/state/index.js
@@ -1,15 +1,12 @@
 // contain the current state of the game
 const states = {
     playerScore: 0,
-    gameSpeed: 3,
+    gameSpeed: 5,
     gameInPlay: false,
     gameOver: false,
-    sensitivity: 0.03,
+    sensitivity: 0.05,
     lock: false,
     lockDown: true,
 }
-setInterval(() => {
-    states.gameSpeed++
-    console.log(states.gameSpeed)
-}, 10000)
+
 export default states
